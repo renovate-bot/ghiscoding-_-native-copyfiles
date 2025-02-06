5c42be3288fa83b025a13e507186234e114d7149
ghiscoding <gbeaulac@gmail.com>
2025-02-05 21:04:08 -0500

    chore: test verbose

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -99,17 +99,6 @@ describe('copyfiles', () => {
     });
   });
 
-  test('flatten', () => {
-    writeFileSync('input/other/a.txt', 'a');
-    writeFileSync('input/b.txt', 'b');
-    writeFileSync('input/other/c.js', 'c');
-    copyfiles(['input/**/*.txt', 'output'], { flat: true }, (err) => {
-      readdir('output', (err, files) => {
-        expect(files).toEqual(['a.txt', 'b.txt']);
-      });
-    });
-  });
-
   test('verbose', () => {
     const logSpy = vi.spyOn(global.console, 'log').mockReturnValue();
     writeFileSync('input/other/a.txt', 'a');
@@ -118,10 +107,21 @@ describe('copyfiles', () => {
     copyfiles(['input/**/*.txt', 'output'], { flat: true, verbose: true }, (err) => {
       readdir('output', (err, files) => {
         expect(files).toEqual(['a.txt', 'b.txt']);
-        // expect(logSpy).toHaveBeenCalledWith('glob found', ['input/b.txt', 'input/other/a.txt']);
+        expect(logSpy).toHaveBeenCalledWith('glob found', ['input/b.txt', 'input/other/a.txt']);
         expect(logSpy).toHaveBeenCalledWith('copy:', { from: 'input/other/a.txt', to: 'output/a.txt' });
         expect(logSpy).toHaveBeenCalledWith('copy:', { from: 'input/b.txt', to: 'output/b.txt' });
         expect(logSpy).toHaveBeenCalledWith('Files copied:   2');
+      });
+    });
+  });
+
+  test('flatten', () => {
+    writeFileSync('input/other/a.txt', 'a');
+    writeFileSync('input/b.txt', 'b');
+    writeFileSync('input/other/c.js', 'c');
+    copyfiles(['input/**/*.txt', 'output'], { flat: true }, (err) => {
+      readdir('output', (err, files) => {
+        expect(files).toEqual(['a.txt', 'b.txt']);
       });
     });
   });
